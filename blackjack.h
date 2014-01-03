36771607971ebda60179945a89a0544b04e89308
Brett Esworthy <brettes19@gmail.com>
2014-01-02 19:50:52 -0500

    Create blackjack.h

---
+++ blackjack.h
@@ -0,0 +1,18 @@
+// blackjack.h - blackjack game class declaration and implementation
+// written by Brett Esworthy
+
+#include <iostream>
+#include <string>
+#include <time.h>
+
+#pragma once
+using namespace std;
+
+struct Blackjack
+{
+	Blackjack(){};
+
+
+
+
+}
