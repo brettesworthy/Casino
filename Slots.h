909cec9d58b50730ca18b8187dde76444ad8d005
Brett Esworthy <brettes19@gmail.com>
2014-01-02 19:55:37 -0500

    Create Slots.h

---
+++ Slots.h
@@ -0,0 +1,16 @@
+// slots.h - slots machine game class declaration and implementation
+// written by Brett Esworthy
+
+#include <iostream>
+#include <string>
+#include <time.h>
+
+#pragma once
+using namespace std;
+
+struct Slots
+{
+	Slots(){};
+
+
+};
